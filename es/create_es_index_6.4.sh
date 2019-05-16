b14ac15723ad3df2c52fed8f7690ed2783b30d96
Amit Saha <amitsaha.in@gmail.com>
2019-05-17 09:37:01 +1000

    Add ES 6.4 scripts

---
+++ es/create_es_index_6.4.sh
@@ -0,0 +1,47 @@
+curl -H 'Content-Type: application/json'  -XPUT "http://elasticsearch:9200/nginx_geoip" -d '
+{
+  "mappings": {
+    "metrics": {
+      "properties": {
+        "@timestamp": {
+          "type": "date"
+        },
+        "geoip": {
+          "properties": {
+            "continent_name": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                }
+              }
+            },
+            "country_iso_code": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                }
+              }
+            },
+            "location": {
+              "type": "geo_point"
+            }
+          }
+        },
+        "real_ip": {
+          "type": "text",
+          "fields": {
+            "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'
--- es/test_event.sh
+++ es/test_event.sh
@@ -1,4 +1,4 @@
 curl -H 'Content-Type: application/json' -XPOST 'http://localhost:9200/nginx_geoip/metrics?pipeline=geoip' -d  \
 '
-{"real_ip":"8.8.8.8","@timestamp":"2019-05-14T23:46:58.282Z"}
+{"real_ip":"49.255.14.118","@timestamp":"2019-05-14T23:46:58.282Z"}
 '
